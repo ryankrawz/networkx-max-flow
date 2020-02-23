2feb68b1988c881df058ca462d3668955f8756ea
Ryan Krawczyk <ryankrawczyk@ryans-macbook.bc.edu>
2020-02-23 16:09:59 -0500

    Max flow logic

--- graph_tests.py
+++ graph_tests.py
@@ -40,28 +40,32 @@ class GraphTestMatrix:
 
     def test_edges(self):
         if (bool(self.empty.g.edges) or
-           set(self.singleton.g.edges) != {(1, 1)} or
-           set(self.small.g.edges) != {(1, 1), (2, 1)} or
-           set(self.medium.g.edges) != {(1, 1), (1, 3), (1, 4), (3, 3), (3, 4), (4, 1), (2, 1), (2, 2)} or
-           set(self.large.g.edges) != {(1, 1), (1, 3), (1, 5), (1, 8), (3, 1), (3, 3), (3, 5), (3, 6),
-                                       (3, 8), (5, 1), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (8, 1),
-                                       (8, 4), (8, 6), (2, 1), (2, 2), (2, 3), (2, 6), (2, 7), (2, 8),
-                                       (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 7), (7, 1), (7, 2),
-                                       (7, 3), (7, 5), (7, 6), (7, 7), (7, 8), (4, 2), (4, 4), (4, 8)} or
-           set(self.completely_connected.g.edges) != {(1, 1), (1, 2), (1, 3), (1, 4), (2, 1), (2, 2),
-                                                      (2, 3), (2, 4), (3, 1), (3, 2), (3, 3), (3, 4),
-                                                      (4, 1), (4, 2), (4, 3), (4, 4)} or
+           set(self.singleton.g.edges) != {('1', '1')} or
+           set(self.small.g.edges) != {('1', '1'), ('2', '1')} or
+           set(self.medium.g.edges) != {('1', '1'), ('1', '3'), ('1', '4'), ('3', '3'), ('3', '4'), ('4', '1'),
+                                        ('2', '1'), ('2', '2')} or
+           set(self.large.g.edges) != {('1', '1'), ('1', '3'), ('1', '5'), ('1', '8'), ('3', '1'), ('3', '3'),
+                                       ('3', '5'), ('3', '6'), ('3', '8'), ('5', '1'), ('5', '4'), ('5', '5'),
+                                       ('5', '6'), ('5', '7'), ('5', '8'), ('8', '1'), ('8', '4'), ('8', '6'),
+                                       ('2', '1'), ('2', '2'), ('2', '3'), ('2', '6'), ('2', '7'), ('2', '8'),
+                                       ('6', '1'), ('6', '2'), ('6', '3'), ('6', '4'), ('6', '5'), ('6', '7'),
+                                       ('7', '1'), ('7', '2'), ('7', '3'), ('7', '5'), ('7', '6'), ('7', '7'),
+                                       ('7', '8'), ('4', '2'), ('4', '4'), ('4', '8')} or
+           set(self.completely_connected.g.edges) != {('1', '1'), ('1', '2'), ('1', '3'), ('1', '4'), ('2', '1'),
+                                                      ('2', '2'), ('2', '3'), ('2', '4'), ('3', '1'), ('3', '2'),
+                                                      ('3', '3'), ('3', '4'), ('4', '1'), ('4', '2'), ('4', '3'),
+                                                      ('4', '4')} or
            bool(self.completely_disconnected.g.edges)):
             raise Exception('incorrect edges reported in GraphTestMatrix.test_edges')
 
     def test_nodes(self):
         if (bool(self.empty.g.nodes) or
-           set(self.singleton.g.nodes) != {1} or
-           set(self.small.g.nodes) != {1, 2} or
-           set(self.medium.g.nodes) != {1, 3, 4, 2} or
-           set(self.large.g.nodes) != {1, 3, 5, 8, 2, 6, 7, 4} or
-           set(self.completely_connected.g.nodes) != {1, 2, 3, 4} or
-           set(self.completely_disconnected.g.nodes) != {1, 2, 3, 4}):
+           set(self.singleton.g.nodes) != {'1'} or
+           set(self.small.g.nodes) != {'1', '2'} or
+           set(self.medium.g.nodes) != {'1', '3', '4', '2'} or
+           set(self.large.g.nodes) != {'1', '3', '5', '8', '2', '6', '7', '4'} or
+           set(self.completely_connected.g.nodes) != {'1', '2', '3', '4'} or
+           set(self.completely_disconnected.g.nodes) != {'1', '2', '3', '4'}):
             raise Exception('incorrect nodes reported in GraphTestMatrix.test_nodes')
 
     def sample_draw(self):
@@ -83,9 +87,21 @@ class GraphTestEdgeList:
         if (set(self.two_nodes.g.edges) != {('1', '2')} or
            set(self.four_nodes.g.edges) != {('4', '2'), ('1', '3'), ('1', '4'), ('3', '4'), ('3', '2')} or
            set(self.nine_nodes.g.edges) != {('3', '6'), ('7', '2'), ('1', '9'), ('3', '8'), ('9', '2'),
-                                           ('8', '7'), ('4', '3'), ('6', '7'), ('5', '7'), ('3', '5'),
-                                           ('1', '4')}):
+                                            ('8', '7'), ('4', '3'), ('6', '7'), ('5', '7'), ('3', '5'),
+                                            ('1', '4')}):
             raise Exception('incorrect edges reported in GraphTestEdgeList.test_edges')
+
+    def test_max_flow_bfs(self):
+        if (self.two_nodes.bfs_max_flow != 3 or
+           self.four_nodes.bfs_max_flow != 4 or
+           self.nine_nodes.bfs_max_flow != 7):
+            raise Exception('incorrect max flow reported in GraphTestEdgeList.test_max_flow_bfs')
+
+    def test_max_flow_dijkstra(self):
+        if (self.two_nodes.dijkstra_max_flow != 3 or
+           self.four_nodes.dijkstra_max_flow != 4 or
+           self.nine_nodes.dijkstra_max_flow != 7):
+            raise Exception('incorrect max flow reported in GraphTestEdgeList.test_max_flow_dijkstra')
 
     def test_nodes(self):
         if (set(self.two_nodes.g.nodes) != {'1', '2'} or
@@ -98,5 +114,8 @@ class GraphTestEdgeList:
 
     def run_tests(self):
         self.test_edges()
+        # TODO: implement BFS algorithm
+        # self.test_max_flow_bfs()
+        self.test_max_flow_dijkstra()
         self.test_nodes()
         print('\nAll tests successful\n')
