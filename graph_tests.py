f43ab6f238cdc07aae378bc1527a0b5b7bbadb98
Ryan Krawczyk <ryankrawczyk@ryans-macbook.bc.edu>
2020-02-23 14:19:26 -0500

    Edge list based graphs

--- graph_tests.py
+++ graph_tests.py
@@ -1,7 +1,7 @@
 from graph_utilities import NetworkXDirectedGraph
 
 
-class GraphTest:
+class GraphTestMatrix:
     def __init__(self):
         self.empty = NetworkXDirectedGraph([])
         self.singleton = NetworkXDirectedGraph([[1]])
@@ -40,32 +40,61 @@ class GraphTest:
 
     def test_edges(self):
         if (bool(self.empty.g.edges) or
-           set(self.singleton.g.edges) != {(0, 0)} or
-           set(self.small.g.edges) != {(0, 0), (1, 0)} or
-           set(self.medium.g.edges) != {(0, 0), (0, 2), (0, 3), (2, 2), (2, 3), (3, 0), (1, 0), (1, 1)} or
-           set(self.large.g.edges) != {(0, 0), (0, 2), (0, 4), (0, 7), (2, 0), (2, 2), (2, 4), (2, 5),
-                                       (2, 7), (4, 0), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (7, 0),
-                                       (7, 3), (7, 5), (1, 0), (1, 1), (1, 2), (1, 5), (1, 6), (1, 7),
-                                       (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 6), (6, 0), (6, 1),
-                                       (6, 2), (6, 4), (6, 5), (6, 6), (6, 7), (3, 1), (3, 3), (3, 7)} or
-           set(self.completely_connected.g.edges) != {(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1),
-                                                      (1, 2), (1, 3), (2, 0), (2, 1), (2, 2), (2, 3),
-                                                      (3, 0), (3, 1), (3, 2), (3, 3)} or
+           set(self.singleton.g.edges) != {(1, 1)} or
+           set(self.small.g.edges) != {(1, 1), (2, 1)} or
+           set(self.medium.g.edges) != {(1, 1), (1, 3), (1, 4), (3, 3), (3, 4), (4, 1), (2, 1), (2, 2)} or
+           set(self.large.g.edges) != {(1, 1), (1, 3), (1, 5), (1, 8), (3, 1), (3, 3), (3, 5), (3, 6),
+                                       (3, 8), (5, 1), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (8, 1),
+                                       (8, 4), (8, 6), (2, 1), (2, 2), (2, 3), (2, 6), (2, 7), (2, 8),
+                                       (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 7), (7, 1), (7, 2),
+                                       (7, 3), (7, 5), (7, 6), (7, 7), (7, 8), (4, 2), (4, 4), (4, 8)} or
+           set(self.completely_connected.g.edges) != {(1, 1), (1, 2), (1, 3), (1, 4), (2, 1), (2, 2),
+                                                      (2, 3), (2, 4), (3, 1), (3, 2), (3, 3), (3, 4),
+                                                      (4, 1), (4, 2), (4, 3), (4, 4)} or
            bool(self.completely_disconnected.g.edges)):
-            raise Exception('incorrect edges reported in GraphTest.test_edges')
+            raise Exception('incorrect edges reported in GraphTestMatrix.test_edges')
 
     def test_nodes(self):
         if (bool(self.empty.g.nodes) or
-           set(self.singleton.g.nodes) != {0} or
-           set(self.small.g.nodes) != {0, 1} or
-           set(self.medium.g.nodes) != {0, 2, 3, 1} or
-           set(self.large.g.nodes) != {0, 2, 4, 7, 1, 5, 6, 3} or
-           set(self.completely_connected.g.nodes) != {0, 1, 2, 3} or
-           set(self.completely_disconnected.g.nodes) != {0, 1, 2, 3}):
-            raise Exception('incorrect nodes reported in GraphTest.test_nodes')
+           set(self.singleton.g.nodes) != {1} or
+           set(self.small.g.nodes) != {1, 2} or
+           set(self.medium.g.nodes) != {1, 3, 4, 2} or
+           set(self.large.g.nodes) != {1, 3, 5, 8, 2, 6, 7, 4} or
+           set(self.completely_connected.g.nodes) != {1, 2, 3, 4} or
+           set(self.completely_disconnected.g.nodes) != {1, 2, 3, 4}):
+            raise Exception('incorrect nodes reported in GraphTestMatrix.test_nodes')
 
     def sample_draw(self):
         self.large.draw_graph()
+
+    def run_tests(self):
+        self.test_edges()
+        self.test_nodes()
+        print('\nAll tests successful\n')
+
+
+class GraphTestEdgeList:
+    def __init__(self):
+        self.two_nodes = NetworkXDirectedGraph('edge_lists/2_nodes.txt', from_edge_list=True)
+        self.four_nodes = NetworkXDirectedGraph('edge_lists/4_nodes.txt', from_edge_list=True)
+        self.nine_nodes = NetworkXDirectedGraph('edge_lists/9_nodes.txt', from_edge_list=True)
+
+    def test_edges(self):
+        if (set(self.two_nodes.g.edges) != {('1', '2')} or
+           set(self.four_nodes.g.edges) != {('4', '2'), ('1', '3'), ('1', '4'), ('3', '4'), ('3', '2')} or
+           set(self.nine_nodes.g.edges) != {('3', '6'), ('7', '2'), ('1', '9'), ('3', '8'), ('9', '2'),
+                                           ('8', '7'), ('4', '3'), ('6', '7'), ('5', '7'), ('3', '5'),
+                                           ('1', '4')}):
+            raise Exception('incorrect edges reported in GraphTestEdgeList.test_edges')
+
+    def test_nodes(self):
+        if (set(self.two_nodes.g.nodes) != {'1', '2'} or
+           set(self.four_nodes.g.nodes) != {'1', '2', '3', '4'} or
+           set(self.nine_nodes.g.nodes) != {'1', '2', '3', '4', '5', '6', '7', '8', '9'}):
+            raise Exception('incorrect nodes reported in GraphTestEdgeList.test_nodes')
+
+    def sample_draw(self):
+        self.nine_nodes.draw_graph()
 
     def run_tests(self):
         self.test_edges()
--- graph_utilities.py
+++ graph_utilities.py
@@ -3,25 +3,34 @@ import networkx as nx
 
 
 class NetworkXDirectedGraph:
-    def __init__(self, adjacency_matrix):
-        self.g = self.produce_graph(adjacency_matrix)
+    def __init__(self, graph_info, from_edge_list=False):
+        if not from_edge_list:
+            self.g = self.produce_graph_matrix(graph_info)
+        else:
+            self.g = self.produce_graph_edge_list(graph_info)
 
     @staticmethod
     # Accepts 2D array representing adjacency matrix, values can be 0 or 1
-    def produce_graph(adjacency_matrix):
+    def produce_graph_matrix(adjacency_matrix):
         directed_graph = nx.DiGraph()
         for i in range(len(adjacency_matrix)):
             # Create node for all nodes in matrix
-            directed_graph.add_node(i)
+            directed_graph.add_node(i + 1)
             for j in range(len(adjacency_matrix[i])):
                 if adjacency_matrix[i][j] == 1:
                     # Create edge for all identified edges in matrix
-                    directed_graph.add_edge(i, j)
+                    directed_graph.add_edge(i + 1, j + 1)
         return directed_graph
+
+    @staticmethod
+    # Accepts file path for text file containing edge list
+    def produce_graph_edge_list(file_path):
+        return nx.read_edgelist(file_path, create_using=nx.DiGraph)
 
     # Renders image of graph using Matplotlib interface
     def draw_graph(self):
-        nx.draw(self.g, with_labels=True, font_weight='bold')
+        pos = nx.spring_layout(self.g)
+        nx.draw(self.g, pos, with_labels=True, font_weight='bold')
         plt.subplot(111)
-        nx.draw_shell(self.g, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+        nx.draw_shell(self.g, with_labels=True, font_weight='bold')
         plt.show()
